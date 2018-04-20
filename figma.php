7ef36c0e1a02f90ab17c097e4256797cc28e71cc
missbamdy <missbusolabamidele@gmail.com>
2018-04-20 21:01:15 +0100

    Add files via upload
    A simple dashboard designed on figma, implemented with css and html.

---
+++ figma.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+$ git init
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <style>
+	
+	
+	body { background:
+      linear-gradient(rgba(12,12,24,0.93),rgba(1,12,24,0.93)),
+      url('pic2.jpg') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+		}
+		
+	#word {text-align: center;
+		font-family: rasa;
+		font-size: 48px;
+		font-style: light;
+		color:#C8EEEB;
+		position: fixed;
+		top: 50px;
+		left: 450px;
+		}
+		
+	#time {text-align: bottom;
+			Font-Family: Rasa;
+			Font-Style: Light;
+			Font-Size: 40px;
+			color:#C8EEEB;
+			position: fixed;
+			bottom: 5px;
+			right: 4px;
+			}
+		
+        </style>
+    <meta charset="utf-8" />
+	
+    <title> HNG internship </title>
+</head>
+
+<div id= "whole">
+<body>
+        <h1 id="word"> It's more than a design </h1>
+		
+		
+        <p id="time"> 
+		<?php
+	
+echo date("Y/m/d") . "<br>";
+echo date("h:i:sa");
+?>
+</footer>
+        
+
+</body>
+</div>
+
+</html>
